6647ae2acbff355ee15d23a684736c9ceaf43031
palooney <lixx3013@umn.edu>
2017-10-24 23:10:58 -0500

    analysis driver update

---
+++ analyze.py
@@ -0,0 +1,28 @@
+from arango import ArangoClient
+
+class analyze:
+	def __init__(self):
+		self.client = ArangoClient(protocol = 'http', host = 'localhost', 
+			port = 8529, username = 'root', password = '', enable_logging = True)
+		self.db = self.client.database('test_db')
+		self.email = self.db.collection("email")
+		self.people = self.db.collection("people")
+	def stat(self):
+		print "----------------------------------------"
+		print "The statistics of email collection is: "
+		for k, v in self.email.statistics().iteritems():
+			print k, v
+		print "----------------------------------------"
+		print "The statistics of people collection is: "
+		for k, v in self.people.statistics().iteritems():
+			print k, v
+		print "----------------------------------------"
+	def sentStat(self):
+		s1 = "FOR x IN email COLLECT people = x._to WITH COUNT INTO counter SORT counter DESC RETURN{people:people, number:counter}"
+		cursor = self.db.aql.execute(s1)
+		print([self.email["_key"] for email in cursor])
+
+
+x = analyze()
+x.stat()
+x.sentStat()
